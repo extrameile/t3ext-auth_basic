6156b0d2114508cedbeeb7dc5f53656daa98380f
Andreas Kießling <andreas@powered-by-coffee.de>
2019-09-23 16:24:30 +0200

    Update ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,7 +4,7 @@ $GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'] = ',authbasic_active,auth
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS'][\TYPO3\CMS\Core\Imaging\IconFactory::class]['overrideIconOverlay'][] = \AndreasKiessling\AuthBasic\Hooks\IconOverlay::class;
 
-// override the repository to fetch additional fields
+// override the repository to fetch additional fields - see https://forge.typo3.org/issues/88300
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Backend\Tree\Repository\PageTreeRepository::class] = [
     'className' => \AndreasKiessling\AuthBasic\Repository\PageTreeRepository::class
 ];
